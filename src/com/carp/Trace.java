1f57ebaae25d9b6dc38bf295aa4a6ff725d66e41
capricorn <8250820+capricorn@users.noreply.github.com>
2018-05-12 17:34:34 -0500

    Add basic method / field vm tracing tool

---
+++ src/com/carp/Trace.java
@@ -0,0 +1,50 @@
+package com.carp;
+
+import com.sun.jdi.Field;
+import com.sun.jdi.VirtualMachine;
+import com.sun.jdi.connect.IllegalConnectorArgumentsException;
+import com.sun.jdi.request.AccessWatchpointRequest;
+import com.sun.jdi.request.MethodEntryRequest;
+import com.sun.jdi.request.ModificationWatchpointRequest;
+
+import java.io.IOException;
+
+public class Trace {
+    private static void setFieldTrace(VirtualMachine vm, String className, String fieldName) {
+        try {
+            Field field = vm.classesByName(className).get(0).fieldByName(fieldName);
+            System.out.println(field.toString());
+            AccessWatchpointRequest accReq = vm.eventRequestManager().createAccessWatchpointRequest(field);
+            ModificationWatchpointRequest modReq = vm.eventRequestManager().createModificationWatchpointRequest(field);
+            MethodEntryRequest methReq = vm.eventRequestManager().createMethodEntryRequest();
+            methReq.addClassFilter("cc");
+            methReq.enable();
+            /*
+            accReq.enable();
+            modReq.enable();
+            */
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        // Only run if diamond client is running already
+        try {
+            VirtualMachine vm = DiamondAPI.attach();
+            //setFieldTrace(vm, "im", "bv");
+            setFieldTrace(vm, "im", "ew");
+            /*
+            System.out.println(vm.process());
+            new Thread(new VMProcIOThread(vm.process().getErrorStream()));
+            new Thread(new VMProcIOThread(vm.process().getInputStream()));
+            */
+            new Thread(new VMEventThread(vm)).start();
+            vm.resume();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (IllegalConnectorArgumentsException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/com/carp/VMEventThread.java
@@ -0,0 +1,57 @@
+package com.carp;
+
+
+import com.sun.jdi.Field;
+import com.sun.jdi.ObjectReference;
+import com.sun.jdi.PrimitiveValue;
+import com.sun.jdi.VirtualMachine;
+import com.sun.jdi.event.*;
+import com.sun.jdi.request.AccessWatchpointRequest;
+import com.sun.jdi.request.MethodEntryRequest;
+import com.sun.jdi.request.ModificationWatchpointRequest;
+
+public class VMEventThread implements Runnable {
+    EventQueue queue;
+    VirtualMachine vm;
+
+    public VMEventThread(VirtualMachine vm) {
+        this.vm = vm;
+        this.queue = vm.eventQueue();
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                EventSet events = queue.remove();
+                EventIterator eventIter = events.eventIterator();
+                while (eventIter.hasNext()) {
+                    //System.out.println("New event");
+                    Event event = eventIter.nextEvent();
+
+                    if (event instanceof ModificationWatchpointEvent) {
+                        ModificationWatchpointEvent modEvent = (ModificationWatchpointEvent) event;
+                        System.out.println("Mod event: " + modEvent.valueToBe());
+                    } else if (event instanceof AccessWatchpointEvent) {
+                        System.out.println("Access request");
+                        AccessWatchpointEvent accEvent = (AccessWatchpointEvent) event;
+                        Field field = accEvent.field();
+                        System.out.println(field.toString());
+                        System.out.println(accEvent.valueCurrent());
+                    } else if (event instanceof MethodEntryEvent) {
+                        MethodEntryEvent methEvent = (MethodEntryEvent) event;
+                        if (methEvent.method().name().equals("a")) {
+                            System.out.println("Method: " + methEvent.toString());
+                            System.out.println(methEvent.method().name());
+                        }
+                    } else if (event instanceof VMDeathEvent || event instanceof VMDisconnectEvent) {
+                        return;
+                    }
+                }
+                events.resume();
+            }
+        } catch (InterruptedException e) {
+
+        }
+    }
+}
