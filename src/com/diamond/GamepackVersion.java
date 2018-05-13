e2d844ec806ec17376e3a7366f2ff1e36c001b4d
carp <8250820+capricorn@users.noreply.github.com>
2018-05-13 16:18:54 -0500

    Add explanation about extracting the version number

--- src/com/diamond/GamepackVersion.java
+++ src/com/diamond/GamepackVersion.java
@@ -17,6 +17,24 @@ public class GamepackVersion {
         }
     }
 
+    /**
+     * Read the current gamepack version from client.class.
+     *
+     * In client.class, at the end of the init (NOT <init>) method, there is a method call
+     * with several parameters passed. The third parameter is the gamepack version,
+     * assuming the class is still completely obfuscated.
+     *
+     * One way to obtain this value is to grab the last instruction of the method, and
+     * traverse backwards until we reach the first return opcode, which occurs just
+     * after the method call we're interested in. Once that's found, it's easy to simply
+     * traverse backwards past the other stack instructions (arguments to the method) until
+     * reaching the instruction that pushes the version number. From here, the number is extracted
+     * and returned.
+     * @param clazz
+     *  Bytes from client.class
+     * @return
+     *  Version of the gamepack
+     */
     public static int getVersion(byte[] clazz) {
         ClassNode client = new ClassNode();
         ClassReader cr = new ClassReader(clazz);
