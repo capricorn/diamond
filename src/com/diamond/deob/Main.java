5f575d449c248711c547388fd267ab601e85e683
carp <8250820+capricorn@users.noreply.github.com>
2018-05-13 22:07:02 -0500

    Only write files to disk, rather than deob anything

--- src/com/diamond/deob/Main.java
+++ src/com/diamond/deob/Main.java
@@ -3,9 +3,6 @@ package com.diamond.deob;
 public class Main {
     public static void main(String[] args) {
         Deobfuscator deob = new Deobfuscator("/tmp/tmp.jar");
-        deob.removeEmptyMethods()
-            .removeCallsToMarkedMethods()
-            .removeEmptyExceptions()
-            .write();
+        deob.write();
     }
 }
